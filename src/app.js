13ec704ca66a7632b6e534cdbe19001b19b250cf
Jacob Ilagan <jilagan5@myseneca.ca>
2025-01-13 15:18:05 -0500

    fixing app.js file

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 // src/app.js
 
-import { signIn, getUser } from "./auth";
+import { signIn, getUser } from './auth';
 
 async function init() {
   // Get our UI elements
-  const userSection = document.querySelector("#user");
-  const loginBtn = document.querySelector("#button");
+  const userSection = document.querySelector('#user');
+  const loginBtn = document.querySelector('#login');
 
-  // Wire up event handlers to deal with login and logout
+  // Wire up event handlers to deal with login and logout.
   loginBtn.onclick = () => {
     // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
-    signIn();
+    signin();
   };
 
-  // See if we're signed in (i.e., we'll have a 'user' object)
+  // See if we're signed in (i.e., we'll have a `user` object)
   const user = await getUser();
   if (!user) {
     return;
@@ -23,11 +23,11 @@ async function init() {
   userSection.hidden = false;
 
   // Show the user's username
-  userSection.querySelector(".username").innerText = user.username;
+  userSection.querySelector('.username').innerText = user.username;
 
-  // Disable the login button
+  // Disable the Login button
   loginBtn.disabled = true;
 }
 
 // Wait for the DOM to be ready, then start the app
-addEventListener("DOMContentLoaded", init);
+addEventListener('DOMContentLoaded', init);
