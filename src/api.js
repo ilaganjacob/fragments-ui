d89ca686a0d1a116fdb491476bfb0d488f37d566
Jacob Ilagan <jilagan5@myseneca.ca>
2025-01-17 14:34:01 -0500

    working on function getUserFragments(user)

--- src/api.js
+++ src/api.js
@@ -1,2 +1,25 @@
 // src/api.js
 
+// fragments microservice api to use, defaults to localhost:8080 if not set in env
+
+const apiUrl = process.env.API_URL || "http://localhost:8080";
+
+/**
+ * Given an authenticated user, request all fragments for this user from the
+ * fragments microservice (currently only running locally). We expect a user
+ * to have an `idToken` attached, so we can send that along with the request.
+ */
+
+export async function getUserFragments(user) {
+  console.log("Requesting user fragments data...");
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments`, {
+      headers: user.authorizationHeaders(),
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`)
+    }
+  } catch (err) {
+    console.log("Unable to call GET /v1/fragments", { err });
+  }
+}
